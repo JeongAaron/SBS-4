9ad718e00e039432f15d6684f00eed76774bab33
kik4817 <kik4817@naver.com>
2025-02-13 19:42:25 +0900

    2025-02-13 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+
+class SingleLinkedList
+{
+private:
+	int size;
+	struct Node
+	{
+		T data;
+		Node* next;
+	};
+	Node* head;
+public:
+	SingleLinkedList()
+	{
+		size = 0;
+		head = nullptr;
+	}
+	void push_front(T data)
+	{
+		Node* newNode = new Node;
+		if (head == nullptr)
+		{
+			head = newNode;
+			newNode->data = data;
+			newNode->next = nullptr;
+		}
+		else
+		{
+			newNode->data = data;
+			newNode->next = head;
+			head = newNode;
+		}
+		size++;
+	}
+	void push_back(T data)
+	{
+		Node* newNode = new Node;
+		Node* currentNode = nullptr;
+		if (head == nullptr)
+		{
+			head = newNode;
+			newNode->data = data;
+			newNode->next = nullptr;
+		}
+		else
+		{
+			newNode->data = data;
+			newNode->next = nullptr;
+		}
+		size++;
+	}
+};
+
+int main()
+{
+	SingleLinkedList<int> single;
+	single.push_front(10);
+	single.push_front(20);
+}
