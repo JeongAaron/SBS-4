ccca7bf3e463b3b94229f694ace7d28944ea9ced
JeongAaron <lovejsk1004@gmail.com>
2025-02-25 20:57:00 +0900

    2025-02-25

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -19,24 +19,61 @@ public:
 	}
 	void resize(int newSize)
 	{
-		return newSize * 2;
-	}
-	void push_back(T data)
-	{
-		if (container == nullptr)
+		if (newSize == 0)
 		{
-			capacity = 1;
-			T* newContainer[capacity] = new T;
-			newContainer[capacity] = data;
+			newSize = 1;
 		}
 		else
 		{
+			newSize = newSize * 2;
+		}
+		T* newContainer = new T[newSize];
+		for (int i = 0; i < size; i++)
+		{
+			newContainer[i] = container[i];
+		}
+		delete[] container;
+		container = newContainer;
+		capacity = newSize;
+	}
+	void push_back(T data)
+	{
+		if (size == capacity)
+		{
+			resize(capacity);
+		}
+		container[size++] = data;
+	}
 
+	void Size()
+	{
+		cout << "Size : "<< size << endl;
+	}
+	void Capacity()
+	{
+		cout << "Capacity : "<< capacity << endl;
+	}
+	void print()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			cout << container[i] << " ";
 		}
 	}
 };
 
 int main()
 {
-
+	Vector<int> vector;
+	vector.push_back(10);
+	vector.push_back(20);
+	vector.push_back(30);
+	vector.push_back(40);
+	vector.push_back(50);
+	vector.push_back(60);
+	vector.push_back(70);
+	vector.push_back(80);
+	vector.Capacity();
+	vector.Size();
+	vector.print();
 }
