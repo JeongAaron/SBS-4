d8485caae85356ae1bb810cd9e5e7e922840a9d2
JeongAaron <lovejsk1004@gmail.com>
2025-02-27 19:40:12 +0900

    2025-02-27

---
+++ Class07th (Vector)/Class07th (Vector).cpp
@@ -0,0 +1,123 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+
+class Vector
+{
+private:
+	int size;
+	int capacity;
+	T* container;
+public:
+	Vector()
+	{
+		size = 0;
+		capacity = 0;
+		container = nullptr;
+	}
+	void resize(int newSize)
+	{
+		if (newSize == 0)
+		{
+			newSize = 1;
+		}
+		else
+		{
+			newSize = newSize * 2;
+		}
+		T* newContainer = new T[newSize];
+		for (int i = 0; i < newSize; i++)
+		{
+			newContainer[i] = NULL;
+		}
+		for (int i = 0; i < size; i++)
+		{
+			newContainer[i] = container[i];
+		}
+		if (container != nullptr)
+		{
+			delete[] container;
+		}
+		container = newContainer;
+		capacity = newSize;
+	}
+	void push_back(T data)
+	{
+		if (size == capacity)
+		{
+			resize(capacity);
+		}
+		container[size++] = data;
+	}
+	void pop_back()
+	{
+		if (size == 0)
+		{
+			cout << "Vector is Empty" << endl;
+		}
+		else
+		{
+			container[--size] = NULL;
+		}
+	}
+	const T& front()
+	{
+		if (size != 0)
+		{
+			return container[0];
+		}
+	}
+	const T& back()
+	{
+		if (size != 0)
+		{
+			return container[size - 1];
+		}
+	}
+	const int& Size()
+	{
+		return size;
+	}
+	const int& Capacity()
+	{
+		return capacity;
+	}
+	const T& operator[](const int& index)
+	{
+		return  container[index];
+	}
+	void begin()
+	{
+		cout << &front() << endl;
+	}
+	~Vector()
+	{
+		if (container != nullptr)
+		{
+			delete[] container;
+		}
+	}
+};
+
+int main()
+{
+	Vector<int> vector;
+	vector.push_back(10);
+	vector.push_back(20);
+	vector.push_back(30);
+	vector.push_back(40);
+	vector.push_back(50);
+	vector.push_back(60);
+	vector.push_back(70);
+	vector.pop_back();
+	cout << "Size : " << vector.Size() << endl;
+	cout << "Capacity : " << vector.Capacity() << endl;
+	cout << "Front : " << vector.front() << endl;
+	cout << "Back : " << vector.back() << endl;
+	for (int i = 0; i < vector.Size(); i++)
+	{
+		cout << vector[i] << " ";
+	}
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,122 +2,66 @@
 
 using namespace std;
 
-template <typename T>
-
-class Vector
+class String 
 {
 private:
+	char* pointer;
 	int size;
-	int capacity;
-	T* container;
 public:
-	Vector()
+	String()
 	{
+		pointer = nullptr;
 		size = 0;
-		capacity = 0;
-		container = nullptr;
 	}
-	void resize(int newSize)
+	void operator = (const char* word)
 	{
-		if (newSize == 0)
+		size = strlen(word) + 1;
+		if (pointer == nullptr)
 		{
-			newSize = 1;
+			pointer = new char[size];
+			for (int i = 0; i < size; i++)
+			{
+				pointer[i] = word[i];
+			}
 		}
 		else
 		{
-			newSize = newSize * 2;
-		}
-		T* newContainer = new T[newSize];
-		for (int i = 0; i < newSize; i++)
-		{
-			newContainer[i] = NULL;
-		}
-		for (int i = 0; i < size; i++)
-		{
-			newContainer[i] = container[i];
-		}
-		if (container != nullptr)
-		{
-			delete[] container;
-		}
-		container = newContainer;
-		capacity = newSize;
-	}
-	void push_back(T data)
-	{
-		if (size == capacity)
-		{
-			resize(capacity);
-		}
-		container[size++] = data;
-	}
-	void pop_back()
-	{
-		if (size == 0)
-		{
-			cout << "Vector is Empty" << endl;
-		}
-		else
-		{
-			container[--size] = NULL;
+			char* newPointer = new char[size];
+			for (int i = 0; i < size; i++)
+			{
+				newPointer[i] = word[i];
+			}
+			delete[] pointer;
+			pointer = newPointer;
 		}
 	}
-	const T& front()
+	const char& operator[](const int& index)
 	{
-		if (size != 0)
+		return  pointer[index];
+	}
+	int Size()
+	{
+		return size - 1;
+	}
+	~String()
+	{
+		if (pointer != nullptr)
 		{
-			return container[0];
-		}
-	}
-	const T& back()
-	{
-		if (size != 0)
-		{
-			return container[size - 1];
-		}
-	}
-	const int& Size()
-	{
-		return size;
-	}
-	const int& Capacity()
-	{
-		return capacity;
-	}
-	const T& operator[](const int& index)
-	{
-		return  container[index];
-	}
-	void begin()
-	{
-		cout << &front() << endl;
-	}
-	~Vector()
-	{
-		if (container != nullptr)
-		{
-			delete[] container;
+			delete[] pointer;
 		}
 	}
 };
-
 int main()
 {
-	Vector<int> vector;
-	vector.push_back(10);
-	vector.push_back(20);
-	vector.push_back(30);
-	vector.push_back(40);
-	vector.push_back(50);
-	vector.push_back(60);
-	vector.push_back(70);
-	vector.pop_back();
-	cout << "Size : " << vector.Size() << endl;
-	cout << "Capacity : " << vector.Capacity() << endl;
-	cout << "Front : " << vector.front() << endl;
-	cout << "Back : " << vector.back() << endl;
-	for (int i = 0; i < vector.Size(); i++)
+	String string;
+	string = "key";
+	for (int i = 0; i < string.Size(); i++)
 	{
-		cout << vector[i] << " ";
+		cout << string[i];
+	}
+	string = "box";
+	for (int i = 0; i < string.Size(); i++)
+	{
+		cout << string[i];
 	}
 }
